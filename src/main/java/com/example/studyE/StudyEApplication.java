bc609edfd201c28308bdd799cf4e954c61fa2d2b
DinhHieu2004 <22130082@st.hcmuaf.edu.vn>
2025-04-18 11:11:17 +0700

    package and pom

---
+++ src/main/java/com/example/studyE/StudyEApplication.java
@@ -0,0 +1,13 @@
+package com.example.studyE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyEApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyEApplication.class, args);
+	}
+
+}
