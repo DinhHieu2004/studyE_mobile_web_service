43d3995a0f6b3ec810d6bce799179642442975fd
haodo04 <22130077@st.hcmuaf.edu.vn>
2025-05-30 20:03:17 +0700

    create topic and lession entity

---
+++ src/main/java/com/example/studyE/entity/Lession.java
@@ -0,0 +1,34 @@
+package com.example.studyE.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "lessions")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Lession {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    private String level;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "topic_id")
+    private Topic topic;
+}
