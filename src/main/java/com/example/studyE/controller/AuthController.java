25618517a12c71b092cc7311f05a505ba3b7cc94
haodo04 <22130077@st.hcmuaf.edu.vn>
2025-06-10 23:57:12 +0700

    fix error pull request

--- src/main/java/com/example/studyE/controller/AuthController.java
+++ src/main/java/com/example/studyE/controller/AuthController.java
@@ -9,6 +9,7 @@ import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
